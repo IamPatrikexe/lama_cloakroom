ff8f238717d77bc73770dcbff680fa117bc5f0ea
Patrik <79817868+Patriks2004hun@users.noreply.github.com>
2022-12-04 01:23:59 +0100

    Add files via upload

---
+++ locales/hu.lua
@@ -0,0 +1,8 @@
+Locales['hu'] = {
+    ['action_msg'] = 'Nyomj ~INPUT_CONTEXT~ gombot, hogy megnyisd a ~b~Ruhatárat',
+    ['changed_msg'] = 'Ruha változtatva.!',
+    ['removed_msg'] = 'Ruha törölve.!',
+
+    ['change_outfit'] = 'Válts ruhát',
+    ['remove_outfit'] = 'Töröld a ruhát',
+}
